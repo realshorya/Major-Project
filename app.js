d4829aa99517c22e0ec6d4e6df6e36830ced2d1b
realshorya <chandrashorya@gmail.com>
2026-05-03 00:55:21 +0530

    updated

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ if(process.env.NODE_ENV!="production"){
 }
 
 const express = require("express");
-const port = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
